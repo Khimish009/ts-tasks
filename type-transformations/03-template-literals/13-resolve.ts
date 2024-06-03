bb1319ddb3b9f1dbfa0d5e9f01f1d2e2bac28e92
Salman <himishevsa@intech.rshb.ru>
2024-06-03 21:32:02 +0300

    add ts challenges

---
+++ type-transformations/03-template-literals/13-resolve.ts
@@ -0,0 +1,14 @@
+type Route = `/${string}`;
+
+export const goToRoute = (route: Route) => {};
+
+// Should succeed:
+
+goToRoute("/users");
+goToRoute("/");
+goToRoute("/admin/users");
+
+// Should error:
+
+goToRoute("users/1");
+goToRoute("http://facebook.com");
